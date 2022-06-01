02cce5b54eac30704426a68c8035b563c5910d60
Alice Gomes Eberhardt <103116442+gh0stumb@users.noreply.github.com>
2022-06-01 10:17:40 -0300

    produtos home
    alterações

--- TeuPC/index.php
+++ TeuPC/index.php
@@ -71,8 +71,58 @@ include_once "includes/header.php";
 
 <br>
 
-<div class="card2" style="width: 18rem;">
-  <img class="card-img-top2" src="imagens/produto1.jpg" alt="Imagem de capa do card">
+
+<!-- PRODUTOS CAROUSEL -->
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+  <!-- The slideshow/carousel -->
+  <div class="carousel-inner1">
+    <div class="carousel-item active">
+      <div class="card2" style="width: 18rem; margin-left: 25px;">
+        <img class="card-img-top2" src="imagens/card1.png" alt="Imagem de capa do card">
+        <div class="card-body2">
+          <h5 class="card-title2">Nome do produto </h5>
+          <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+          <p class="card-text2-preco">R$ 1500</p>
+          <a href="#" class="btn btn-primary2">Comprar</a>
+        </div>
+      </div>
+
+      <div class="card2" style="width: 18rem; margin-left: 25px;">
+        <img class="card-img-top2" src="imagens/produto1.jpg" alt="Imagem de capa do card">
+        <div class="card-body2">
+          <h5 class="card-title2">Nome do produto </h5>
+          <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+          <p class="card-text2-preco">R$ 1500</p>
+          <a href="#" class="btn btn-primary2">Comprar</a>
+        </div>
+      </div>
+
+      <div class="card2" style="width: 18rem; margin-left: 25px;">
+        <img class="card-img-top2" src="imagens/produto1.jpg" alt="Imagem de capa do card">
+        <div class="card-body2">
+          <h5 class="card-title2">Nome do produto </h5>
+          <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+          <p class="card-text2-preco">R$ 1500</p>
+          <a href="#" class="btn btn-primary2">Comprar</a>
+        </div>
+      </div>
+
+      <div class="card2" style="width: 18rem; margin-left: 25px;">
+        <img class="card-img-top2" src="imagens/card1.png" alt="Imagem de capa do card">
+        <div class="card-body2">
+          <h5 class="card-title2">Nome do produto </h5>
+          <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+          <p class="card-text2-preco">R$ 1500</p>
+          <a href="#" class="btn btn-primary2">Comprar</a>
+        </div>
+      </div>
+    </div>
+  
+
+    <div class="carousel-item">
+    <div class="card2" style="width: 18rem;">
+  <img class="card-img-top2" src="imagens/card1.png" alt="Imagem de capa do card">
   <div class="card-body2">
     <h5 class="card-title2">Nome do produto </h5>
     <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
@@ -83,7 +133,7 @@ include_once "includes/header.php";
 </div>
 
 
-<div class="card2" style="width: 18rem;">
+<div class="card2" style="width: 18rem; margin-left: 25px;">
   <img class="card-img-top2" src="imagens/produto1.jpg" alt="Imagem de capa do card">
   <div class="card-body2">
     <h5 class="card-title2">Nome do produto </h5>
@@ -94,7 +144,7 @@ include_once "includes/header.php";
   </div>
 </div>
 
-<div class="card2" style="width: 18rem;">
+<div class="card2" style="width: 18rem; margin-left: 25px;">
   <img class="card-img-top2" src="imagens/produto1.jpg" alt="Imagem de capa do card">
   <div class="card-body2">
     <h5 class="card-title2">Nome do produto </h5>
@@ -105,8 +155,8 @@ include_once "includes/header.php";
   </div>
 </div>
 
-<div class="card2" style="width: 18rem;">
-  <img class="card-img-top2" src="imagens/produto1.jpg" alt="Imagem de capa do card">
+<div class="card2" style="width: 18rem; margin-left: 25px;">
+  <img class="card-img-top2" src="imagens/card1.png" alt="Imagem de capa do card">
   <div class="card-body2">
     <h5 class="card-title2">Nome do produto </h5>
     <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
@@ -114,6 +164,19 @@ include_once "includes/header.php";
     <a href="#" class="btn btn-primary2">Comprar</a>
 
   </div>
+</div>
+
+    </div>
+  </div>
+
+  <!-- Left and right controls/icons -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next1" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
+</div>
 </div>
 
 
