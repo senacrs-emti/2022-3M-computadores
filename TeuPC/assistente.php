e279fcbd0945b1921c3298841b66a554bcf6f4aa
HumbertoCG18 <humberto.cgomes17@gmail.com>
2022-06-08 08:40:04 -0300

    assistente

--- TeuPC/assistente.php
+++ TeuPC/assistente.php
@@ -4,9 +4,29 @@ include_once "includes/header.php";
 ?>
 
 
+<div class= "main">
+<br>
+<br>
+<br>
+<br>
+<br>
 
 
+<center>
+    <h1 style>Vamos começar, nos diga como você quer seu computador</h1>
+    <br>
+    <h3>Qual processador?</h3>
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+  <label class="btn btn-secondary active">
+    <input type="radio" name="options" id="option1" autocomplete="off" checked> Intel </input>
+  </label>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option2" autocomplete="off"> AMD </input>
+  </label>
+<center>
 
+</div>
+</div>
 
 
 <div class="footer1">
