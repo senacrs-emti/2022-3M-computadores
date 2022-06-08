1fc534c3d3ab932ebed366caf9375bdcf1dccf49
AlexisSonic55 <103124674+AlexisSonic55@users.noreply.github.com>
2022-06-08 08:03:53 -0300

    Create botoes.php

---
+++ TeuPC/botoes.php
@@ -0,0 +1,23 @@
+<?php
+include_once "includes/head.php";
+include_once "includes/header.php";
+?>
+<div class= "main"> 
+<div class="row">
+
+
+
+</div>
+</div>
+
+
+
+<div class="footer1">
+
+<?php
+
+include_once "includes/footer.php";
+
+?>
+
+</div>
