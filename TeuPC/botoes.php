ac6d01285b0278da0bbcd6ee247bbb0cadca16bf
AlexisSonic55 <103124674+AlexisSonic55@users.noreply.github.com>
2022-06-08 08:18:18 -0300

    Update botoes.php

--- TeuPC/botoes.php
+++ TeuPC/botoes.php
@@ -5,7 +5,20 @@ include_once "includes/header.php";
 <div class= "main"> 
 <div class="row">
 
+<div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+  <button type="button" class="btn btn-secondary">1</button>
+  <button type="button" class="btn btn-secondary">2</button>
 
+  <div class="btn-group" role="group">
+    <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Dropdown
+    </button>
+    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+      <a class="dropdown-item" href="#">Dropdown link</a>
+      <a class="dropdown-item" href="#">Dropdown link</a>
+    </div>
+  </div>
+</div>
 
 </div>
 </div>
