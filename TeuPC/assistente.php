ba27566439ca26308037f31be0f807471d1193c4
AlexisSonic55 <103124674+AlexisSonic55@users.noreply.github.com>
2022-06-08 09:00:53 -0300

    Update assistente.php

--- TeuPC/assistente.php
+++ TeuPC/assistente.php
@@ -19,6 +19,57 @@ include_once "includes/header.php";
 </div>
 </div>
 
+<br>
+
+<center>
+
+    <br>
+    <h3>Placa de vídeo dedicada?</h3>
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+  <label class="btn btn-secondary active">
+    <input type="radio" name="options" id="intel" autocomplete="off" checked> Sim </input>
+  </label>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="amd" autocomplete="off"> Não </input>
+  </label>
+<center>
+</div>
+</div>
+
+<br>
+
+<center>
+
+    <br>
+    <h3>Como seria o pagamento?</h3>
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+  <label class="btn btn-secondary active">
+    <input type="radio" name="options" id="intel" autocomplete="off" checked> A prazo </input>
+  </label>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="amd" autocomplete="off"> Á vista </input>
+  </label>
+<center>
+</div>
+</div>
+
+<br>
+
+<center>
+
+    <br>
+    <h3>Qual seria o preço desejado?</h3>
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+  <div class="btn-group" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-secondary">Left</button>
+  <button type="button" class="btn btn-secondary">Middle</button>
+  <button type="button" class="btn btn-secondary">Right</button>
+</div>
+  </div>
+
+<center>
+</div>
+</div>
 
 <div class="footer1">
 
