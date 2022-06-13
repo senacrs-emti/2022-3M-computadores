5ef3b430fab57f41eb4f2f674786798049094f4f
AlexisSonic55 <103124674+AlexisSonic55@users.noreply.github.com>
2022-06-13 09:05:58 -0300

    Update assistente.php

--- TeuPC/assistente.php
+++ TeuPC/assistente.php
@@ -10,46 +10,40 @@ include_once "includes/header.php";
     <h1 style>Como você deseja o TEU PC ?</h1>
     <br>
     <h3>Qual processador?</h3>
-<div class="btn-group btn-group-toggle" data-toggle="buttons">
+
+    <div class="btn-group btn-group-toggle" data-toggle="buttons">
   <label class="btn btn-secondary active">
-  <div>
-  <label>
-    <input type="checkbox" class="radio" value="1" name="fooby[1][]" />Intel</label>
-  <label>
-    <input type="checkbox" class="radio2" value="1" name="fooby[1][]" />AMD</label>
-</div>
+    <input type="radio" name="options" id="option1" autocomplete="off" checked> Intel
   </label>
-</div>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option2" autocomplete="off"> AMD
+  </label>
 </div>
 
 <br>
     <br>
     <h3>Placa de vídeo dedicada?</h3>
+
     <div class="btn-group btn-group-toggle" data-toggle="buttons">
   <label class="btn btn-secondary active">
-  <div>
-  <label>
-    <input type="checkbox" class="radio" value="1" name="fooby[2][]" />Sim</label>
-  <label>
-    <input type="checkbox" class="radio2" value="1" name="fooby[2][]" />Não</label>
-</div>
+    <input type="radio" name="options" id="option1" autocomplete="off" checked> Sim
   </label>
-</div>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option2" autocomplete="off"> Não
+  </label>
 </div>
 
 <br>
     <br>
     <h3>Como seria o pagamento?</h3>
+
     <div class="btn-group btn-group-toggle" data-toggle="buttons">
   <label class="btn btn-secondary active">
-  <div>
-  <label>
-    <input type="checkbox" class="radio" value="1" name="fooby[3][]" />A prazo</label>
-  <label>
-    <input type="checkbox" class="radio2" value="1" name="fooby[3][]" />Á vista</label>
-</div>
+    <input type="radio" name="options" id="option1" autocomplete="off" checked> A Prazo
   </label>
-</div>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option2" autocomplete="off"> Á Vista
+  </label>
 </div>
 
 <br>
@@ -57,18 +51,17 @@ include_once "includes/header.php";
     <h3>Qual seria o preço desejado?</h3>
     <div class="btn-group btn-group-toggle" data-toggle="buttons">
   <label class="btn btn-secondary active">
-  <div>
-  <label>
-    <input type="checkbox" class="radio" value="1" name="fooby[4][]" />3mil</label>
-  <label>
-    <input type="checkbox" class="radio2" value="1" name="fooby[4][]" />5mil</label>
-  <label>
-    <input type="checkbox" class="radio2" value="1" name="fooby[4][]" />7mil</label>
-    <label>
-    <input type="checkbox" class="radio2" value="1" name="fooby[4][]" />10mil+</label>
-</div>
+    <input type="radio" name="options" id="option1" autocomplete="off" checked> 3mil
   </label>
-</div>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option2" autocomplete="off"> 5mil
+  </label>
+  <label class="btn btn-secondary active">
+    <input type="radio" name="options" id="option3" autocomplete="off"> 7mil
+  </label>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option4" autocomplete="off"> 10mil+
+  </label>
 </div>
 
 <div class="has-text-centered">
