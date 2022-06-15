a7f67055e85b7660b7f5b4523bf023570b89f19f
AlexisSonic55 <103124674+AlexisSonic55@users.noreply.github.com>
2022-06-15 07:59:28 -0300

    Update produtos.php

--- TeuPC/produtos.php
+++ TeuPC/produtos.php
@@ -4,6 +4,121 @@ include_once "includes/head.php";
 include_once "includes/header.php";
 ?>
 
+<!-- PRODUTOS CAROUSEL -->
+<div id="demo2" class="carousel slide" data-bs-ride="carousel">
+
+  <!-- The slideshow/carousel -->
+<?php 
+foreach($teupc as $valor)
+
+?>
+
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="card card2" style="width: 18rem; margin-left: -5px;">
+        <img class="card-img-top2" src="imagens/card1.png" alt="Imagem de capa do card">
+        <div class="card-body2">
+          <h5 class="card-title2">Nome do produto </h5>
+          <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+          <p class="card-text2-preco">R$ 1500</p>
+          <a href="#" class="btn btn-primary2">Comprar</a>
+        </div>
+      </div>
+
+      <div class="card card2" style="width: 18rem; margin-left: 15px;">
+        <img class="card-img-top2" src="imagens/produto1.jpg" alt="Imagem de capa do card">
+        <div class="card-body2">
+          <h5 class="card-title2">Nome do produto </h5>
+          <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+          <p class="card-text2-preco">R$ 1500</p>
+          <a href="#" class="btn btn-primary2">Comprar</a>
+        </div>
+      </div>
+
+      <div class="card card2" style="width: 18rem; margin-left: 20px;">
+        <img class="card-img-top2" src="imagens/produto1.jpg" alt="Imagem de capa do card">
+        <div class="card-body2">
+          <h5 class="card-title2">Nome do produto </h5>
+          <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+          <p class="card-text2-preco">R$ 1500</p>
+          <a href="#" class="btn btn-primary2">Comprar</a>
+        </div>
+      </div>
+
+      <div class="card card2" style="width: 18rem; margin-left: 20px;">
+        <img class="card-img-top2" src="imagens/card1.png" alt="Imagem de capa do card">
+        <div class="card-body2">
+          <h5 class="card-title2">Nome do produto </h5>
+          <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+          <p class="card-text2-preco">R$ 1500</p>
+          <a href="#" class="btn btn-primary2">Comprar</a>
+        </div>
+      </div>
+    </div>
+  
+ 
+
+
+
+
+    <div class="carousel-item">
+    <div class="card card2" style="width: 18rem; margin-left: -5px;">
+  <img class="card-img-top2" src="imagens/card1.png" alt="Imagem de capa do card">
+  <div class="card-body2">
+    <h5 class="card-title2">Nome do produto </h5>
+    <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+    <p class="card-text2-preco">R$ 1500</p>
+    <a href="#" class="btn btn-primary2">Comprar</a>
+
+  </div>
+</div>
+
+
+<div class="card card2" style="width: 18rem; margin-left: 25px;">
+  <img class="card-img-top2" src="imagens/produto1.jpg" alt="Imagem de capa do card">
+  <div class="card-body2">
+    <h5 class="card-title2">Nome do produto </h5>
+    <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+    <p class="card-text2-preco">R$ 1500</p>
+    <a href="#" class="btn btn-primary2">Comprar</a>
+
+  </div>
+</div>
+
+<div class="card card2" style="width: 18rem; margin-left: 25px;">
+  <img class="card-img-top2" src="imagens/produto1.jpg" alt="Imagem de capa do card">
+  <div class="card-body2">
+    <h5 class="card-title2">Nome do produto </h5>
+    <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+    <p class="card-text2-preco">R$ 1500</p>
+    <a href="#" class="btn btn-primary2">Comprar</a>
+
+  </div>
+</div>
+
+<div class="card card2" style="width: 18rem; margin-right: 10px;">
+  <img class="card-img-top2" src="imagens/card1.png" alt="Imagem de capa do card">
+  <div class="card-body2">
+    <h5 class="card-title2">Nome do produto </h5>
+    <p class="card-text2">Descrição e informações detalhadas do produto e estou enchendo esse texto para se ter uma ideia de como ficaria</p>
+    <p class="card-text2-preco">R$ 1500</p>
+    <a href="#" class="btn btn-primary2">Comprar</a>
+
+  </div>
+</div>
+
+    </div>
+  </div>
+
+  <!-- Left and right controls/icons -->
+  <button class="carousel-control-prev prev2" type="button" data-bs-target="#demo2" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next next2" type="button" data-bs-target="#demo2" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
+</div>
+</div>
 
 
 <?php
