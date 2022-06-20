27a7c24c14aa1ae07e5931d31dbf4339842ce1a9
AlexisSonic55 <103124674+AlexisSonic55@users.noreply.github.com>
2022-06-20 09:07:17 -0300

    Update produtos.php

--- TeuPC/produtos.php
+++ TeuPC/produtos.php
@@ -12,6 +12,7 @@ include_once "includes/header.php";
 foreach($teupc as $valor)
 
 ?>
+
 </div>
 <?php
 $sql = "SELECT * FROM pcpronto";    
@@ -24,13 +25,17 @@ $resultado = mysqli_query($conn, $sql);
 
 
   <div class="carousel-inner">
+  <div class="carousel-item active">
+    
 
-    <div class="carousel-item active">
-
-      <div class="card card2" style="width: 18rem; margin-left: -5px;" class="col-sm-3 col-md-3 col-lg-3 col-xl-3">
+  
 <?php
          $pcs = 1;
+         $fecha = 1;
          while($row = mysqli_fetch_array($resultado)){
+         
+
+       echo '<div class="card card2" style="width: 18rem; margin-left: -5px;" class="col-sm-3 col-md-3 col-lg-3 col-xl-3">';   
        echo '<img class="card-img-top2"src="imagens/'.$row['Imagem'].'" alt="Imagem de capa do card">';
        echo '<div class="card-body2">';
        echo  '<h5 class="card-title2">'.$row['Nome'].'</h5>';
@@ -38,19 +43,27 @@ $resultado = mysqli_query($conn, $sql);
             <p class="card-text2">'.$row['PlacaVD'].'</p>
             <p class="card-text2-preco">R$'.$row['Preco'].'</p>';
        echo     '<a href="'.$row['produtoID'].'" class="btn btn-primary2">Comprar</a>';
+       echo '</div>';
+       echo '<br>';
+       echo '<br>';
+       echo '</div>';
+          $fecha++;
           $pcs++; 
-         }
+         };
           ?>
           
-        </div>
+        
 
       </div>
- 
+      <button class="carousel-control-prev prev2" type="button" data-bs-target="#demo2" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next next2" type="button" data-bs-target="#demo2" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
     </div>
 
   </div>
-
-
 
 <?php
 
