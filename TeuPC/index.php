7d3e261ded8a40ae1ad01399ea6ad85bb5e1fb48
fmcampanaro <rodrigo.ssalvadori@gmail.com>
2022-06-20 09:03:13 -0300

    update integração

--- TeuPC/index.php
+++ TeuPC/index.php
@@ -91,13 +91,16 @@ $resultado = mysqli_query($conn, $sql);
 
 
   <div class="carousel-inner">
+  <div class="carousel-item active">
+    
 
-    <div class="carousel-item active">
-
-      
+  
 <?php
          $pcs = 1;
+         $fecha = 1;
          while($row = mysqli_fetch_array($resultado)){
+         
+
        echo '<div class="card card2" style="width: 18rem; margin-left: -5px;" class="col-sm-3 col-md-3 col-lg-3 col-xl-3">';   
        echo '<img class="card-img-top2"src="imagens/'.$row['Imagem'].'" alt="Imagem de capa do card">';
        echo '<div class="card-body2">';
@@ -106,14 +109,24 @@ $resultado = mysqli_query($conn, $sql);
             <p class="card-text2">'.$row['PlacaVD'].'</p>
             <p class="card-text2-preco">R$'.$row['Preco'].'</p>';
        echo     '<a href="'.$row['produtoID'].'" class="btn btn-primary2">Comprar</a>';
+       echo '</div>';
+       echo '<br>';
+       echo '<br>';
+       echo '</div>';x
+          $fecha++;
           $pcs++; 
-         }
+         };
           ?>
           
-        </div>
+        
 
       </div>
- 
+      <button class="carousel-control-prev prev2" type="button" data-bs-target="#demo2" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next next2" type="button" data-bs-target="#demo2" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
     </div>
 
   </div>
