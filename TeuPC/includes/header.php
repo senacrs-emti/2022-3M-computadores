35f240354904c80b2b213a2e090be34b0244d2b2
HumbertoCG18 <humberto.cgomes17@gmail.com>
2022-06-27 11:46:28 -0300

    Site

--- TeuPC/includes/header.php
+++ TeuPC/includes/header.php
@@ -7,7 +7,26 @@
   <a href="index.php">TEU PC</a>
   <a href="assistente.php">Assistente</a>
   <a href="produtos.php">PCs e Notebooks</a>
-  <a href="lista.php?cat=1">Peças</a>
+
+  <div class="dropdown">
+    <button class="dropbtn">Peças
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="lista.php?cat=1">Processadores</a>
+      <a href="lista.php?cat=2">Placa Mãe</a>
+      <a href="lista.php?cat=3">Memoria Ram</a>
+      <a href="lista.php?cat=4">SSD</a>
+      <a href="lista.php?cat=5">HD</a>
+      <a href="lista.php?cat=6">Cooler</a>
+      <a href="lista.php?cat=7">Placa De Video</a>
+      <a href="lista.php?cat=8">Fontes</a>
+      <a href="lista.php?cat=9">Gabinete</a>
+      <a href="lista.php?cat=10">Notebook</a>
+      <a href="lista.php?cat=11">PC Montados</a>
+    </div>
+  </div>
+
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
 
   </a>
