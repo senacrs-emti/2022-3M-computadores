de9f2f8ffc7b77f8686db00d478bfc8c7dd134eb
Humberto Corrêa <humberto.cgomes17@gmail.com>
2022-06-26 22:58:46 -0300

    site

--- TeuPC/index.php
+++ TeuPC/index.php
@@ -68,7 +68,7 @@ include_once "includes/header.php";
 </div>
 
 <div class="button1">
-<a href="lista.php?cat=1" class="btn btn-primary">Monte Teu Pc</a>
+<a href="resultado-assistente.php" class="btn btn-primary">Monte Teu Pc</a>
 </div>
 
 <br>
@@ -80,27 +80,27 @@ include_once "includes/header.php";
 </div>
 <?php
 $sql = "SELECT * FROM pecas WHERE CategoriaID=1";
+
+
 $resultado = mysqli_query($conn, $sql);
 ?>        
-
   
 <?php
          $pecas = 1;
          $fecha = 1;
          while($row = mysqli_fetch_array($resultado)){
-         
-
-       echo '<div class="card card2" style="width: 19rem; margin-left: -5px;" class="col-sm-3 col-md-3 col-lg-3 col-xl-3">';   
+       echo '<div class="card2" style="width: 17.5rem; margin-left: 10px;" class="col-sm-3 col-md-3 col-lg-3 col-xl-3">';   
        echo '<img class="card-img-top2"src="imagens/'.$row['Imagem'].'" alt="Imagem de capa do card">';
-       echo '<div class="card-body2" style="height:250px;">';
-       echo '<h5 class="card-title2">'.$row['Nome'].'</h5><p class="card-text2-preco">R$ '.$row['Preco'].'</p>';
-       echo '<a href="'.$row['Link'].'" class="btn btn-primary2">Comprar</a>';
+       echo '<div class="card-body2" style="height:265px; width:265px">';
+       echo '<h5 class="card-title2">'.$row['Nome'].'</h5>';
+       echo '<a href="'.$row['Link'].'" class="btn btn-primary2">Comprar</a>'.'<p>'.'<br>'.'R$ '.$row['Preco'].'</p>';
        echo '</div>';
        echo '<br>';
        echo '<br>';
        echo '</div>';
-          $fecha++;
-          $pecas++; 
+       $pecas++; 
+       $fecha++;
+
          };
           ?>
           
