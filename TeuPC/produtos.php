05aeb2f3bc3447f037eef08d8c282da401e538c5
Humberto Corrêa <humberto.cgomes17@gmail.com>
2022-06-26 23:18:19 -0300

    Site

--- TeuPC/produtos.php
+++ TeuPC/produtos.php
@@ -17,11 +17,11 @@ $resultado2 = mysqli_query($conn, $pcpronto);
          $pcs = 1;
          $fecha = 1;
          while($row = mysqli_fetch_array($resultado)){
-          echo '<div class="card card2" style="width: 19rem; margin-left: -5px; margin-top:10px" class="col-sm-3 col-md-3 col-lg-3 col-xl-3">';   
+          echo '<div class="card2" style="width: 17.5rem; margin-left: 10px;" class="col-sm-3 col-md-3 col-lg-3 col-xl-3">';   
           echo '<img class="card-img-top2"src="imagens/'.$row['Imagem'].'" alt="Imagem de capa do card">';
-          echo '<div class="card-body2" style="height:250px;">';
-          echo '<h5 class="card-title2">'.$row['Nome'].'</h5><p class="card-text2-preco">R$ '.$row['Preco'].'</p>';
-          echo '<a href="'.$row['Link'].'" class="btn btn-primary2">Comprar</a>';
+          echo '<div class="card-body2" style="height:265px; width:265px">';
+          echo '<h5 class="card-title2">'.$row['Nome'].'</h5>';
+          echo '<a href="'.$row['Link'].'" class="btn btn-primary2">Comprar</a>'.'<p>'.'<br>'.'R$ '.$row['Preco'].'</p>';
           echo '</div>';
           echo '<br>';
           echo '<br>';
@@ -35,23 +35,21 @@ $resultado2 = mysqli_query($conn, $pcpronto);
          $pcs = 1;
          $fecha = 1;
          while($row = mysqli_fetch_array($resultado2)){
-          echo '<div class="card card2" style="width: 19rem; margin-left: -5px; margin-top:10px" class="col-sm-3 col-md-3 col-lg-3 col-xl-3">';   
-          echo '<img class="card-img-top2"src="imagens/'.$row['Imagem'].'" alt="Imagem de capa do card">';
-          echo '<div class="card-body2" style="height:250px;">';
-          echo '<h5 class="card-title2">'.$row['Nome'].'</h5><p class="card-text2-preco">R$ '.$row['Preco'].'</p>';
-          echo '<a href="'.$row['Link'].'" class="btn btn-primary2">Comprar</a>';
-          echo '</div>';
-          echo '<br>';
-          echo '<br>';
-          echo '</div>';
+       echo '<div class="card2" style="width: 17.5rem; margin-left: 10px;" class="col-sm-3 col-md-3 col-lg-3 col-xl-3">';   
+       echo '<img class="card-img-top2"src="imagens/'.$row['Imagem'].'" alt="Imagem de capa do card">';
+       echo '<div class="card-body2" style="height:265px; width:265px">';
+       echo '<h5 class="card-title2">'.$row['Nome'].'</h5>';
+       echo '<a href="'.$row['Link'].'" class="btn btn-primary2">Comprar</a>'.'<p>'.'<br>'.'R$ '.$row['Preco'].'</p>';
+       echo '</div>';
+       echo '<br>';
+       echo '<br>';
+       echo '</div>';
           $fecha++;
           $pcs++; 
          };
 ?>
           
 
-<div class="footer">
 <?php
 include_once "includes/footer.php";
 ?>
-</div>
