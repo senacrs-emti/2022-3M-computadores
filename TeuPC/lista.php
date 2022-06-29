19a5f94556f90dc3f202ae67e2325f66d6cbf3d2
HumbertoCG18 <humberto.cgomes17@gmail.com>
2022-06-29 09:37:07 -0300

    site

--- TeuPC/lista.php
+++ TeuPC/lista.php
@@ -26,40 +26,43 @@ ON p.CategoriaID = c.CategoriaID
 AND pc.CategoriaID = c.CategoriaID
 INNER JOIN campos AS cp
 ON cp.CampoID = pc.CampoID
-WHERE c.CategoriaID = $codigo";
+WHERE c.CategoriaID = $codigo LIMIT 1";
+$sql2="SELECT * FROM categorias WHERE CategoriaID = $codigo";
 
 $resultado = mysqli_query( $conn , $sql);
-
+$resultado2 = mysqli_query( $conn , $sql2);
+$resultado3 = mysqli_query( $conn , $sql2);
 ?>
 
-<?php
-$tabLista = 1 ; 
-if($resultado){
-  while($row = mysqli_fetch_array($resultado)){
-    echo '<br>'.'<center>'.'<h2 style="color: white;">'.$row['NomeCategoria'].'</h2>'.'</center>';
-    echo '<tbody>'.'<tr>'.'<th scope="row">1</th>'.'<td>'.'<h5 style="color:white;">'.$row['NomePeca'].'</td>'.'<td>'.'</td>'.'<td>'.$row['Descricao'].'</td>'.'<td>'.$row['Descricao'].'</td>'.'<td>'.$row['Descricao'].'</td>'.'<td>'.'<h5 style="color:white;">'.'R$ '.$row['PrecoPeca'].'</h5>'.'</td>'.'<td>'.'<img style="height:60px; width:60px; background-image:none ;" src="imagens/'.$row['ImagemPeca'].'".>'.'</td>'.'</tr>'.'</tbody>';
-
-
-  } 
-}
-?>
 
 <div class="container"> 
+  <?php 
+  if($resultado){
+    while($row = mysqli_fetch_array($resultado))
+    echo '<br>'.'<center>'.'<h2 style="color: white;">'.$row['NomeCategoria'].'</h2>'.'</center>';
+  }
+  
+  ?>
 <table class="table" style="color: white">
   <thead>
-      <th scope="col">ID</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Socket</th>
-      <th scope="col">Frequencia</th>
-      <th scope="col">Nucleos</th>
-      <th scope="col">Consumo</th>
-      <th scope="col">Preço</th>
-      <th scope="col">Imagem</th>
-  </thead>
+<?php 
+$row3 = mysqli_fetch_array($resultado3);
 
+print_r($row3);
+
+
+while ($row2 = mysqli_fetch_array($resultado2)) { ?>
+  <th scope="col"><?php if ($row2['Descricao'] == 'CpuID') {
+   echo 'Marca';
+  }else{ echo $row2['Descricao'] ;}
+   ?></th>
+  <?php 
+  } 
+  ?>
+
+  </thead>
 </table>
 </div>
-
     
 
 
