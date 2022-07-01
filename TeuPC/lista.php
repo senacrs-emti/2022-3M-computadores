0cc2feeddd5936561bb2387012927cfd93c45bad
Humberto Corrêa <humberto.cgomes17@gmail.com>
2022-06-30 21:51:12 -0300

    Update lista.php

--- TeuPC/lista.php
+++ TeuPC/lista.php
@@ -9,16 +9,16 @@ include_once "includes/header.php";
 
 $codigo = $_GET["cat"];
 
-$sql="SELECT 
-p.PecaID,
-p.Preco,
+$sql="SELECT
 p.Imagem AS ImagemPeca,
 p.Preco AS PrecoPeca,
 p.Nome AS NomePeca,
 c.Nome AS NomeCategoria,
+c.CategoriaID AS CategoriaID, 
 cp.Nome AS NomeCampo,
-pc.DadoCampo AS Descricao
-FROM `pecascampos` AS pc
+pc.DadoCampo AS DadoCampo,
+p.PecaID
+FROM pecascampos AS pc
 INNER JOIN pecas AS p
 ON pc.PecaID = p.PecaID
 INNER JOIN categorias AS c
@@ -31,39 +31,19 @@ $sql2="SELECT * FROM categorias WHERE CategoriaID = $codigo";
 
 $resultado = mysqli_query( $conn , $sql);
 $resultado2 = mysqli_query( $conn , $sql2);
-$resultado3 = mysqli_query( $conn , $sql2);
 ?>
 
 
-<div class="container"> 
   <?php 
   if($resultado){
-    while($row = mysqli_fetch_array($resultado))
-    echo '<br>'.'<center>'.'<h2 style="color: white;">'.$row['NomeCategoria'].'</h2>'.'</center>';
+    while($row = mysqli_fetch_array($resultado)){
+      echo '<br>'.'<center>'.'<h2 style="color: white;">'.$row['NomeCategoria'].'</h2>'.'</center>';
+      echo '<div class="container">'.'<table class="table" style="color: white">'.'<thead>'.'<th scope="col">'.'</th>'.'<th scope="col">'.$row['NomeCampo'].'</th>'.'<th scope="col">'.$row['NomeCampo'].'</th>'.'<th scope="col">'.$row['NomeCampo'].'</th>'.'<th scope="col">'.$row['NomeCampo'].'</th>'.'<th scope="col">Consumo</th>'.'<th scope="col">Preço</th>'.'<th scope="col">Imagem</th>'.'</thead>';
+      echo '<tbody>'.'<tr>'.'<th scope="row">1</th>'.'<td>'.$row['NomePeca'].'</td>'.'<td>'.$row['DadoCampo '].'</td>'.'<td>'.$row['DadoCampo'].'</td>'.'<td>'.$row['DadoCampo'].'</td>'.'<td>'.$row['DadoCampo'].'</td>'.'<td>'.'R$ '.$row['PrecoPeca'].'</td>'.'<td>'.'<img style="height:60px; width:60px; background-image:none ;" src="imagens/'.$row['Descricao'].'".>'.'</td>'.'</tr>'.'</tbody>'.'</table>'.'</div>';
+
+    }
   }
-  
-  ?>
-<table class="table" style="color: white">
-  <thead>
-<?php 
-$row3 = mysqli_fetch_array($resultado3);
-
-print_r($row3);
-
-
-while ($row2 = mysqli_fetch_array($resultado2)) { ?>
-  <th scope="col"><?php if ($row2['Descricao'] == 'CpuID') {
-   echo 'Marca';
-  }else{ echo $row2['Descricao'] ;}
-   ?></th>
-  <?php 
-  } 
-  ?>
-
-  </thead>
-</table>
-</div>
-    
+?>
 
 
 <?php
