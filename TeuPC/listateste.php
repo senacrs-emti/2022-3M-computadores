8864f3941de14bda93064fd48cc2d551ee3a0723
Rodrigo Salvadori <103116372+Salvadorri@users.noreply.github.com>
2022-06-30 23:06:33 -0300

    testes

--- TeuPC/listateste.php
+++ TeuPC/listateste.php
@@ -8,17 +8,18 @@ include_once "includes/header.php";
 <?php
 
 $codigo = $_GET["cat"];
+$codigo2 = $_GET["cate"];
 
-$sql="SELECT 
-p.PecaID,
-p.Preco,
+$sql="SELECT
 p.Imagem AS ImagemPeca,
 p.Preco AS PrecoPeca,
 p.Nome AS NomePeca,
 c.Nome AS NomeCategoria,
+c.CategoriaID AS CategoriaID, 
 cp.Nome AS NomeCampo,
-pc.DadoCampo AS Descricao
-FROM `pecascampos` AS pc
+pc.DadoCampo AS DadoCampo,
+p.PecaID
+FROM pecascampos AS pc
 INNER JOIN pecas AS p
 ON pc.PecaID = p.PecaID
 INNER JOIN categorias AS c
@@ -26,41 +27,28 @@ ON p.CategoriaID = c.CategoriaID
 AND pc.CategoriaID = c.CategoriaID
 INNER JOIN campos AS cp
 ON cp.CampoID = pc.CampoID
-WHERE c.CategoriaID = $codigo";
+WHERE c.CategoriaID = $codigo AND p.PecaID = $codigo2";
+
+
 
 $resultado = mysqli_query( $conn , $sql);
-
+$row2 = mysqli_fetch_array($resultado);
+echo '<br>'.'<center>'.'<h2 style="color: white;">'.$row2['NomeCategoria'].'</h2>'.'</center>';
 ?>
 
-<?php
-$tabLista = 1 ; 
-if($resultado){
-  while($row = mysqli_fetch_array($resultado)){
-    echo '<br>'.'<center>'.'<h2 style="color: white;">'.$row['NomeCategoria'].'</h2>'.'</center>';
-    echo '<tbody>'.'<tr>'.'<th scope="row">1</th>'.'<td>'.'<h5 style="color:white;">'.$row['NomePeca'].'</td>'.'<td>'.'</td>'.'<td>'.$row['Descricao'].'</td>'.'<td>'.$row['Descricao'].'</td>'.'<td>'.$row['Descricao'].'</td>'.'<td>'.'<h5 style="color:white;">'.'R$ '.$row['PrecoPeca'].'</h5>'.'</td>'.'<td>'.'<img style="height:60px; width:60px; background-image:none ;" src="imagens/'.$row['ImagemPeca'].'".>'.'</td>'.'</tr>'.'</tbody>';
+
+  <?php 
 
 
-  } 
-}
+  if($resultado){
+    while($row = mysqli_fetch_array($resultado)){
+
+      echo '<div class="container">'.'<table class="table" style="color: white">'.'<thead>'.'<th scope="col">'.'</th>'.'<th scope="col">'.$row['NomeCampo'].'</th>'.'<th scope="col">'.$row['NomeCampo'].'</th>'.'<th scope="col">'.$row['NomeCampo'].'</th>'.'<th scope="col">'.$row['NomeCampo'].'</th>'.'<th scope="col">Consumo</th>'.'<th scope="col">Preço</th>'.'<th scope="col">Imagem</th>'.'</thead>';
+      echo '<tbody>'.'<tr>'.'<th scope="row">1</th>'.'<td>'.$row['NomePeca'].'</td>'.'<td>'.$row['DadoCampo '].'</td>'.'<td>'.$row['DadoCampo'].'</td>'.'<td>'.$row['DadoCampo'].'</td>'.'<td>'.$row['DadoCampo'].'</td>'.'<td>'.'R$ '.$row['PrecoPeca'].'</td>'.'<td>'.'<img style="height:60px; width:60px; background-image:none ;" src="imagens/'.$row['Descricao'].'".>'.'</td>'.'</tr>'.'</tbody>'.'</table>'.'</div>';
+
+    }
+  }
 ?>
-
-<div class="container"> 
-<table class="table" style="color: white">
-  <thead>
-      <th scope="col">ID</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Socket</th>
-      <th scope="col">Frequencia</th>
-      <th scope="col">Nucleos</th>
-      <th scope="col">Consumo</th>
-      <th scope="col">Preço</th>
-      <th scope="col">Imagem</th>
-  </thead>
-
-</table>
-</div>
-
-    
 
 
 <?php
