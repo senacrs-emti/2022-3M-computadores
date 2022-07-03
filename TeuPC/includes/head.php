17fd9c44e6e7a65d3cfb3a09acf5a6e0856c149c
Humberto Corrêa <humberto.cgomes17@gmail.com>
2022-07-03 19:20:51 -0300

    Update head.php

--- TeuPC/includes/head.php
+++ TeuPC/includes/head.php
@@ -9,5 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.5">
     <title>TeuPC</title>    
+
+    <link rel="icon" href="imagens/monke.jpg" type="image/x-icon" />
 </head>
 <body>
