3cd78b7547e2e5b21bda9d33619eb76241517ed6
Humberto <humberto.cgomes17@gmail.com>
2022-07-04 09:05:51 -0300

    Update footer.php

--- TeuPC/includes/footer.php
+++ TeuPC/includes/footer.php
@@ -31,26 +31,7 @@
     </section>
 
     <section class="footer2">
-      <form action="">
-        <div class="row d-flex justify-content-center">
-          <div class="col-auto">
-            <p class="pt-2">
-              <strong>Fique atualizado</strong>
-            </p>
-          </div>
-          <div class="col-md-5 col-12">
-            <div class="form-outline form-white mb-4">
-              <input type="email" id="form5Example21" class="form-control" />
-              <label class="form-label" for="form5Example21">Email</label>
-            </div>
-          </div>
-          <div class="col-auto">
-            <button type="submit" class="btn btn-outline-light mb-4">
-              Inscrever
-            </button>
-          </div>
-        </div>
-      </form>
+      
     </section>
   </div>
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
